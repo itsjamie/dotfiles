79f724bd073340886de85bfb33f48ced669209a3
Jamie Stackhouse <contin673@gmail.com>
2015-01-23 01:05:03 -0400

    Last update for a bit I think? Probably won't be though.

--- install-deps.sh
+++ install-deps.sh
@@ -2,3 +2,12 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 
 ./brew.sh
 ./brew-cash.sh
+
+# for the c alias (syntax highlighted cat)
+sudo easy_install Pygments
+
+# change to bash 4 (installed by homebrew)
+BASHPATH=$(brew --prefix)/bin/bash
+sudo echo $BASHPATH >> /etc/shells
+chsh -s $BASHPATH # will set for current user only.
+echo $BASH_VERSION # should be 4.x not the old 3.2.X
