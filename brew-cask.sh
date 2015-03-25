257ce8737d9c04d7fa4bdfffee2b705c9f2f2b97
Jamie Stackhouse <itsjamie.stackhouse@gmail.com>
2015-03-24 22:32:04 -0300

    Remove Mou, and update Git user.

--- brew-cask.sh
+++ brew-cask.sh
@@ -6,7 +6,6 @@ brew tap caskroom/versions
 brew cask install alfred
 brew cask install google-chrome
 brew cask install mailbox
-brew cask install mou
 brew cask install iterm2
 brew cask install sublime-text3
 brew cask install spectacle
