503a62f86001c49a5c69e9025b0d7772b39c0a31
Jamie Stackhouse <(none)>
2016-03-27 13:39:00 -0300

    Update Brew cask apps.

--- brew-cask.sh
+++ brew-cask.sh
@@ -1,13 +1,9 @@
 # Install native apps
-
 brew install caskroom/cask/brew-cask
 brew tap caskroom/versions
-
 brew cask install alfred
 brew cask install google-chrome
-brew cask install mailbox
 brew cask install iterm2
-brew cask install sublime-text3
 brew cask install spectacle
 brew cask install spotify
 brew cask install vlc
