8b99ad15cd32a4dd80fe3797eac8604c73b08ad9
Jamie Stackhouse <contin673@gmail.com>
2021-12-31 13:56:16 -0400

    Update cask install.

--- brew-cask.sh
+++ brew-cask.sh
@@ -1,9 +1,2 @@
 # Install native apps
-brew install caskroom/cask/brew-cask
-brew tap caskroom/versions
-brew cask install alfred
-brew cask install google-chrome
-brew cask install iterm2
-brew cask install spectacle
-brew cask install spotify
-brew cask install vlc
+brew install --cask alfred iterm2 rectangle google-chrome spotify vlc
